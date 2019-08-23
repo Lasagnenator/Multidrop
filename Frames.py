4b5d87e54782c76596df5242149aeb77e7ae8482
Lasagnenator <38394321+Lasagnenator@users.noreply.github.com>
2019-08-23 22:49:15 +1000

    Updated windows and added placeholder functions
    Still need to figure out the event handling for the send button

--- Frames.py
+++ Frames.py
@@ -17,13 +17,16 @@ import wx.xrc
 class FileSendFrame ( wx.Frame ):
 
     def __init__( self, parent ):
-        wx.Frame.__init__ ( self, parent, id = wx.ID_ANY, title = u"Send File", pos = wx.DefaultPosition, size = wx.Size( 800,300 ), style = wx.DEFAULT_FRAME_STYLE|wx.TAB_TRAVERSAL )
+        wx.Frame.__init__ ( self, parent, id = wx.ID_ANY, title = u"Send File", pos = wx.DefaultPosition, size = wx.Size( -1,-1 ), style = wx.CAPTION|wx.CLOSE_BOX|wx.MINIMIZE_BOX|wx.RESIZE_BORDER|wx.SYSTEM_MENU|wx.TAB_TRAVERSAL )
 
         self.SetSizeHints( wx.DefaultSize, wx.DefaultSize )
 
         bSizer6 = wx.BoxSizer( wx.VERTICAL )
 
         bSizer4 = wx.BoxSizer( wx.HORIZONTAL )
+
+        self.ScanButton = wx.Button( self, wx.ID_ANY, u"Scan", wx.DefaultPosition, wx.DefaultSize, wx.BU_EXACTFIT )
+        bSizer4.Add( self.ScanButton, 0, wx.ALL|wx.ALIGN_CENTER_VERTICAL, 5 )
 
         self.m_staticText3 = wx.StaticText( self, wx.ID_ANY, u"Select file", wx.DefaultPosition, wx.DefaultSize, 0 )
         self.m_staticText3.Wrap( -1 )
@@ -33,30 +36,48 @@ class FileSendFrame ( wx.Frame ):
         self.m_filePicker1 = wx.FilePickerCtrl( self, wx.ID_ANY, wx.EmptyString, u"Select a file", u"*.*", wx.DefaultPosition, wx.DefaultSize, wx.FLP_OPEN|wx.FLP_SMALL )
         bSizer4.Add( self.m_filePicker1, 0, wx.ALL, 5 )
 
+        self.m_staticText5 = wx.StaticText( self, wx.ID_ANY, u"File:", wx.DefaultPosition, wx.Size( 200,-1 ), wx.ALIGN_CENTER_HORIZONTAL|wx.ST_ELLIPSIZE_MIDDLE )
+        self.m_staticText5.Wrap( -1 )
+
+        bSizer4.Add( self.m_staticText5, 0, wx.ALL|wx.ALIGN_CENTER_VERTICAL, 5 )
+
 
         bSizer6.Add( bSizer4, 0, wx.ALIGN_CENTER_HORIZONTAL, 5 )
 
-        self.m_scrolledWindow1 = wx.ScrolledWindow( self, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.HSCROLL|wx.VSCROLL )
+        self.m_scrolledWindow1 = wx.ScrolledWindow( self, wx.ID_ANY, wx.DefaultPosition, wx.Size( 420,160 ), wx.HSCROLL|wx.VSCROLL )
         self.m_scrolledWindow1.SetScrollRate( 5, 5 )
-        bSizer5 = wx.BoxSizer( wx.VERTICAL )
-
-        self.m_panel1 = SendToPanel( self.m_scrolledWindow1, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.TAB_TRAVERSAL )
-        bSizer5.Add( self.m_panel1, 0, wx.ALL, 5 )
+        self.bSizer5 = wx.BoxSizer( wx.VERTICAL )
 
 
-        self.m_scrolledWindow1.SetSizer( bSizer5 )
+        self.m_scrolledWindow1.SetSizer( self.bSizer5 )
         self.m_scrolledWindow1.Layout()
-        bSizer5.Fit( self.m_scrolledWindow1 )
-        bSizer6.Add( self.m_scrolledWindow1, 1, wx.EXPAND |wx.ALL, 5 )
+        bSizer6.Add( self.m_scrolledWindow1, 1, wx.EXPAND, 5 )
 
 
         self.SetSizer( bSizer6 )
         self.Layout()
+        bSizer6.Fit( self )
 
         self.Centre( wx.BOTH )
 
+        # Connect Events
+        self.Bind( wx.EVT_SHOW, self.OnShow )
+        self.ScanButton.Bind( wx.EVT_BUTTON, self.ScanNearby )
+        self.m_filePicker1.Bind( wx.EVT_FILEPICKER_CHANGED, self.FileChanged )
+
     def __del__( self ):
         pass
+
+
+    # Virtual event handlers, overide them in your derived class
+    def OnShow( self, event ):
+        event.Skip()
+
+    def ScanNearby( self, event ):
+        event.Skip()
+
+    def FileChanged( self, event ):
+        event.Skip()
 
 
 ###########################################################################
@@ -70,24 +91,48 @@ class SendToPanel ( wx.Panel ):
 
         bSizer6 = wx.BoxSizer( wx.HORIZONTAL )
 
-        self.ReceiverName = wx.TextCtrl( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, wx.TE_READONLY|wx.BORDER_DEFAULT )
-        bSizer6.Add( self.ReceiverName, 0, wx.ALL|wx.ALIGN_CENTER_VERTICAL, 5 )
+        self.ReceiverName = wx.StaticText( self, wx.ID_ANY, u"name", wx.DefaultPosition, wx.Size( 100,-1 ), wx.ALIGN_LEFT|wx.ST_ELLIPSIZE_END )
+        self.ReceiverName.Wrap( -1 )
+
+        bSizer6.Add( self.ReceiverName, 0, wx.ALIGN_CENTER_VERTICAL, 5 )
 
         self.ProgressGuage = wx.Gauge( self, wx.ID_ANY, 100, wx.DefaultPosition, wx.DefaultSize, wx.GA_HORIZONTAL )
         self.ProgressGuage.SetValue( 0 )
-        bSizer6.Add( self.ProgressGuage, 1, wx.ALL|wx.ALIGN_CENTER_VERTICAL, 5 )
+        bSizer6.Add( self.ProgressGuage, 1, wx.ALIGN_CENTER_VERTICAL, 5 )
 
-        self.m_button5 = wx.Button( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.Size( 64,64 ), 0|wx.BORDER_NONE )
+        self.SendButton = wx.Button( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.Size( 64,64 ), 0|wx.BORDER_NONE )
 
-        self.m_button5.SetBitmap( wx.Bitmap( u"send.png", wx.BITMAP_TYPE_ANY ) )
-        bSizer6.Add( self.m_button5, 0, wx.ALL, 5 )
+        self.SendButton.SetBitmap( wx.Bitmap( u"send.png", wx.BITMAP_TYPE_ANY ) )
+        bSizer6.Add( self.SendButton, 0, wx.TOP|wx.BOTTOM, 5 )
+
+        self.DeniedLabel = wx.StaticText( self, wx.ID_ANY, u"Denied", wx.DefaultPosition, wx.DefaultSize, 0 )
+        self.DeniedLabel.Wrap( -1 )
+
+        self.DeniedLabel.Hide()
+
+        bSizer6.Add( self.DeniedLabel, 0, wx.ALL|wx.ALIGN_CENTER_VERTICAL, 5 )
+
+        self.AcceptedLabel = wx.StaticText( self, wx.ID_ANY, u"Accepted", wx.DefaultPosition, wx.DefaultSize, 0 )
+        self.AcceptedLabel.Wrap( -1 )
+
+        self.AcceptedLabel.Hide()
+
+        bSizer6.Add( self.AcceptedLabel, 0, wx.ALL|wx.ALIGN_CENTER_VERTICAL, 5 )
 
 
         self.SetSizer( bSizer6 )
         self.Layout()
 
+        # Connect Events
+        self.SendButton.Bind( wx.EVT_BUTTON, self.ButtonClick )
+
     def __del__( self ):
         pass
+
+
+    # Virtual event handlers, overide them in your derived class
+    def ButtonClick( self, event ):
+        event.Skip()
 
 
 ###########################################################################
--- client.py
+++ client.py
@@ -3,9 +3,30 @@ from funcs import *
 import wx
 import Frames
 
+AvailablePeople = []
+
+def AppendPersonToSend(Frame, name):
+    AvailablePeople.append(Frames.SendToPanel( Frame.m_scrolledWindow1, wx.ID_ANY, wx.DefaultPosition, wx.Size( 400,80 ), wx.TAB_TRAVERSAL ))
+    Frame.bSizer5.Add( AvailablePeople[-1], 0, wx.ALIGN_CENTER_HORIZONTAL, 5 )
+    AvailablePeople[-1].SetLabel(name)
+    AvailablePeople[-1].ButtonClick = SendClicked
+
+def SendClicked(event):
+    print("here")
+
 class FileSend(Frames.FileSendFrame):
     def __init__(self, parent):
         Frames.FileSendFrame.__init__(self, parent)
+        for panel in AvailablePeople:
+            panel.ButtonClick = SendClicked
+    def ButtonClick(self, event):
+        pass
+    def OnShow(self, event):
+        AppendPersonToSend(self, "person")
+    def FileChanged(self, event):
+        pass
+    def ScanNearby(self, event):
+        self.bSizer5.Clear(delete_windows=True)
 
 locale = wx.Locale.GetSystemLanguage()
 app = wx.App()
